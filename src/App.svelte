df06a0b06a8d9aee4b8b8f9577f355f6772a761c
Mohammed AbdulRahman <mohammed.abdulrahman@apryse.com>
2024-10-03 12:20:01 +0300

    localhost loads successfully
    - license:
       a. PDFTron -> Apryse
       b. year
       c. links re-direction.
    - package.json: latest dependencies versions.
    - App.svelte: corrected loadDocument call.
    - copy-webviewer-file.js: import instead of constants
    - rollup.config.js:
       a. package names correction.
       b. configure svelte using compilerOptions.
    - readme:
       a. PDFTron -> Apryse
       b. links re-direction.

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
 
 	const ready = (r) => {
 		const instance = r.detail.instance;
-		instance.loadDocument("https://pdftron.s3.amazonaws.com/downloads/pl/PDFTRON_about.pdf");
+		instance.UI.loadDocument("https://pdftron.s3.amazonaws.com/downloads/pl/PDFTRON_about.pdf");
 	}
 	
 </script>
